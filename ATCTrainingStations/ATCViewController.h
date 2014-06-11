1c9f56a3c3841055312de687f3fe038484e8f2da
jan@appzman.com <jan@appzman.com>
2014-06-11 14:55:01 -0500

    Initial Commit

---
+++ ATCTrainingStations/ATCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATCViewController.h
+//  ATCTrainingStations
+//
+//  Created by Janusz Chudzynski on 6/11/14.
+//  Copyright (c) 2014 Janusz Chudzynski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATCViewController : UIViewController
+
+@end
---
+++ ATCTrainingStations/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ATCTrainingStations
+//
+//  Created by Janusz Chudzynski on 6/11/14.
+//  Copyright (c) 2014 Janusz Chudzynski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ATCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ATCAppDelegate class]));
+    }
+}
